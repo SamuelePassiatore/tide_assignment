17b5625ba10a284533349f1f300be85c405a4891
SamuelePassiatore <semmisno2@protonmail.com>
2024-06-17 14:31:53 +0200

    added web3 library with connect wallet function

--- my-vue-app/src/components/AppHeader.vue
+++ my-vue-app/src/components/AppHeader.vue
@@ -1,10 +1,73 @@
 <script>
+import Web3 from 'web3';
+
 export default {
-    name: "AppHeader",
+    name: 'AppHeader',
+    data() {
+        return {
+            web3: null,
+            accounts: [],
+            userAddress: null,
+            isConnected: false
+        };
+    },
     methods: {
+        async connectWallet() {
+            if (typeof window.ethereum !== 'undefined') {
+                try {
+                    // Request account access
+                    this.web3 = new Web3(window.ethereum);
+                    this.accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                    this.userAddress = this.accounts[0];
+                    this.isConnected = true;
+
+                    console.log('Connected account:', this.userAddress);
+                } catch (error) {
+                    console.error('Error connecting to wallet:', error);
+                    alert('An error occurred while connecting to the wallet.');
+                }
+            } else {
+                console.error('MetaMask is not installed!');
+                alert('MetaMask is not installed! Please install MetaMask and try again.');
+            }
+        },
         logout() {
-            // Redirect to login page
+            this.web3 = null;
+            this.accounts = [];
+            this.userAddress = null;
+            this.isConnected = false;
+
+            alert('You have been logged out of the application. Please disconnect your wallet from MetaMask for complete security.');
             this.$router.push({ name: 'login' });
+        }
+    },
+    computed: {
+        formatUserAddress() {
+            if (this.userAddress) {
+                const firstChars = this.userAddress.substring(0, 6); // mostra i primi 6 caratteri
+                const lastChars = this.userAddress.substring(this.userAddress.length - 4); // mostra gli ultimi 4 caratteri
+                return `${firstChars}...${lastChars}`;
+            } else {
+                return '';
+            }
+        }
+    },
+    mounted() {
+        // Check if already connected on component mount
+        if (typeof window.ethereum !== 'undefined') {
+            window.ethereum.request({ method: 'eth_accounts' })
+                .then(accounts => {
+                    if (accounts.length > 0) {
+                        this.accounts = accounts;
+                        this.userAddress = this.accounts[0];
+                        this.isConnected = true;
+
+                        console.log('Wallet already connected:', this.userAddress);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error checking wallet connection:', error);
+                });
         }
     }
 };
@@ -17,8 +80,12 @@ export default {
             <span class="name ps-3 fs-3" href="#">TIDE</span>
         </div>
         <div id="navbarNavAltMarkup">
-            <div class="navbar-nav d-flex">
-                <button class="btn btn-primary me-2">Connect wallet</button>
+            <div class="navbar-nav d-flex align-items-center">
+                <button v-if="!isConnected" class="btn btn-primary me-4" @click="connectWallet">Connect wallet</button>
+                <div v-if="isConnected" class="me-4">
+                    <img src="../../public/metamask.png" alt="metamask" class="logo-wallet me-1">
+                    <span class="me-2 text-white">{{ formatUserAddress }}</span>
+                </div>
                 <button class="btn btn-danger" @click="logout">Log out</button>
             </div>
         </div>
@@ -37,5 +104,9 @@ nav {
 
 .name {
     color: white;
+}
+
+.logo-wallet {
+    height: 30px;
 }
 </style>
